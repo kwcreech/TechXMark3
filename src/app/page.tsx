60a7f1c3ab49070e4cb42d1371ea9f71864e4437
Kerry Creech <kerrywcreech@gmail.com>
2024-03-16 19:50:17 -0400

    Created sidebar, stories, and suggested accounts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,245 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen bg-[#141414]">
+      <div className="flex flex-row ml-7 pt-3">
+        
+        // Sidebar
+        <nav className="text-white border-r-2 border-neutral-700">
+          <h1 className="text-3xl mb-20 mr-7">Instagram</h1>
+          <ul>
+            <li className="mb-3">Home</li>
+            <li className="mb-3">Search</li>
+            <li className="mb-3">Explore</li>
+            <li className="mb-3">Reels</li>
+            <li className="mb-3">Messages</li>
+            <li className="mb-3">Notifications</li>
+            <li className="mb-3">Create</li>
+            <li className="mb-12">Profile</li>
+            <li className="mb-3">Threads</li>
+            <li>More</li>
+          </ul>
+        </nav>
+        
+        // Stories + first post on feed
+        <div className="ml-10 flex flex-col">
+          // Stories
+          <div className="flex flex-row space-x-5">
+            
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">
+                <Image
+                  src="/images/aasalogo.JPEG"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full w-full h-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>uncaasa</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/uncbasketball.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>uncmbb</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/boat.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>lilyachty</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/panthers.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>panthers</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/dcthedon.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>dcthedon</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/shai.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>shai</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="image-wrapper w-16 h-16 rounded-full p-1 bg-gradient-to-r from-yellow-400 to-pink-600">  
+                <Image
+                  src="/images/maye.jpg"
+                  width={55}
+                  height={55}
+                  alt="Profile Picture"
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-white text-xs pt-1 flex justify-center">
+                <p>drakemaye</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        // Suggested accounts
+        <div className="flex flex-col ml-28">
+          
+          <div className="flex flex-row">  
+            <div>
+              <Image
+                src="/images/pfp.jpeg"
+                width={55}
+                height={55}
+                alt="Profile Picture"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col ml-3 justify-center">
+              <p className="text-white text-sm">kcreech_</p>
+              <p className="text-gray-600 text-xs">Kerry Creech</p>
+            </div>
+            <div className="flex items-center ml-14 text-blue-800 text-sm">
+              <p>Switch</p>
+            </div>
+          </div>
+          
+          <div className="flex flex-row space-x-20 mt-10">
+            <p className="text-gray-400 text-sm">Suggested for you</p>
+            <p className="text-white text-sm">See all</p>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="flex flex-row mt-5">  
+            <div>
+              <Image
+                src="/images/tana.jpg"
+                width={55}
+                height={55}
+                alt="Profile Picture"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col ml-3 justify-center">
+              <p className="text-white text-sm">tana</p>
+              <p className="text-gray-400 text-xs">Suggested for you</p>
+            </div>
+            <div className="flex text-blue-800 text-sm ml-8 items-center">
+              <p>Follow</p>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="flex flex-row mt-5">  
+            <div>
+              <Image
+                src="/images/bryceyoung.jpg"
+                width={55}
+                height={55}
+                alt="Profile Picture"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col ml-3 justify-center">
+              <p className="text-white text-sm">bryceyoung</p>
+              <p className="text-gray-400 text-xs">Suggested for you</p>
+            </div>
+            <div className="flex text-blue-800 text-sm ml-8 items-center">
+              <p>Follow</p>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="flex flex-row mt-5">  
+            <div>
+              <Image
+                src="/images/tennistv.jpg"
+                width={55}
+                height={55}
+                alt="Profile Picture"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col ml-3 justify-center">
+              <p className="text-white text-sm">tennistv</p>
+              <p className="text-gray-400 text-xs">Suggested for you</p>
+            </div>
+            <div className="flex text-blue-800 text-sm ml-8 items-center">
+              <p>Follow</p>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-row mt-5">  
+            <div>
+              <Image
+                src="/images/kyrgios.jpg"
+                width={55}
+                height={55}
+                alt="Profile Picture"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col ml-3 justify-center">
+              <p className="text-white text-sm">kyrgios</p>
+              <p className="text-gray-400 text-xs">Suggested for you</p>
+            </div>
+            <div className="flex text-blue-800 text-sm ml-8 items-center">
+              <p>Follow</p>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </main>
+    
   );
 }
